4653789741ed20cba7d36d96036866fff0b6d998
collins anaba <evulukwu@me.com>
2020-04-19 14:47:21 -0500

    new swift app created

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Collins Evulukwu on 4/6/20.
+//  Copyright © 2020 Collins Anaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
